25352eb6636debdc2d165bc568005fd99cf6b6b3
Justin Good <jgoodhcg@gmail.com>
2025-06-28 00:44:12 -0400

    Add Rust LSP

--- init.lua
+++ init.lua
@@ -1043,6 +1043,7 @@ require('lazy').setup({
         ts_ls = {},
         --
 
+        rust_analyzer = {},
         clojure_lsp = {},
 
         -- Add JSON language server
@@ -1356,7 +1357,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'clojure' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'clojure', 'rust' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
