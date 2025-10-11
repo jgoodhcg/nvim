83b0aef7e0799c388b8154d9ba0e4f67c7c6f139
Justin Good <jgoodhcg@gmail.com>
2025-10-10 23:02:04 -0400

    Add db viewer

--- init.lua
+++ init.lua
@@ -582,6 +582,27 @@ require('lazy').setup({
       vim.g['conjure#log#winheight'] = 12
     end,
   },
+  -- Database UI and helpers
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      {
+        'kristijanhusak/vim-dadbod-completion',
+        ft = { 'sql', 'mysql', 'plsql' },
+        lazy = true,
+      },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
   -- Structural editing with Paredit (slurp and barf)
   {
     'guns/vim-sexp',
